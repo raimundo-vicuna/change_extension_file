5b9b3a7f731c68a7e8b4bf3a9d46a8da57ced8c7
Raimundo <98242461+raimundo-vicuna@users.noreply.github.com>
2025-08-03 18:38:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 
 carpeta = ""
-
+extension = ""
 for nombre_archivo in os.listdir(carpeta):
     ruta_completa = os.path.join(carpeta, nombre_archivo)
     
     if os.path.isfile(ruta_completa):
         nombre_sin_ext = os.path.splitext(nombre_archivo)[0]
         
-        nuevo_nombre = nombre_sin_ext + ".jpg"
+        nuevo_nombre = nombre_sin_ext + extension
         nueva_ruta = os.path.join(carpeta, nuevo_nombre)
 
         os.rename(ruta_completa, nueva_ruta)
