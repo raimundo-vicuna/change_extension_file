6c9eeaae6a26e8ff36f7af1064aef04a669e7d48
raimundo-vicuna <raivicuna@gmail.com>
2025-08-03 18:35:15 -0400

    v1

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+carpeta = ""
+
+for nombre_archivo in os.listdir(carpeta):
+    ruta_completa = os.path.join(carpeta, nombre_archivo)
+    
+    if os.path.isfile(ruta_completa):
+        nombre_sin_ext = os.path.splitext(nombre_archivo)[0]
+        
+        nuevo_nombre = nombre_sin_ext + ".jpg"
+        nueva_ruta = os.path.join(carpeta, nuevo_nombre)
+
+        os.rename(ruta_completa, nueva_ruta)
+        print(f"Renombrado: {nombre_archivo} → {nuevo_nombre}")
